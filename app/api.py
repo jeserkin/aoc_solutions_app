8f835b8d532c37224a8c0f1e70bc0733d1e3a1bc
Eugene Serkin <jeserkin@gmail.com>
2023-01-05 12:39:05 +0200

    #3: Added Django-ninja framework support (#4)

---
+++ app/api.py
@@ -0,0 +1,7 @@
+from ninja import NinjaAPI
+
+api = NinjaAPI(
+    title='Advent of Code 2022- solutions',
+    description='Solutions for 2022- problems implemented in `Python`',
+    version='1.0.0'
+)
