cefb3d9e44c4ddf7df3a21407d941a5d52b597ed
Eugene Serkin <jeserkin@gmail.com>
2023-01-09 16:51:27 +0200

    #7: Added solution for 2022 Day 4 problem
    * Resolved first part of Day 4 problem

--- y2022/api.py
+++ y2022/api.py
@@ -4,7 +4,7 @@ from ninja import Router, File
 from ninja.files import UploadedFile
 
 from y2022.models import Solution
-from y2022.service import Day1Resolver, Day2Resolver, Day3Resolver
+from y2022.service import Day1Resolver, Day2Resolver, Day3Resolver, Day4Resolver
 
 router = Router(tags=["2022"])
 
@@ -33,4 +33,13 @@ def day3_solution(request, problem_input: UploadedFile = File(...)):
     Solves Day 3 problem and provides solution for both parts
     """
     resolver = Day3Resolver()
+    return list(resolver.resolve(problem_input))
+
+
+@router.post('/day/4', response=List[Solution], summary='Day 4 solutions')
+def day4_solution(request, problem_input: UploadedFile = File(...)):
+    """
+    Solves Day 4 problem and provides solution for both parts
+    """
+    resolver = Day4Resolver()
     return list(resolver.resolve(problem_input))
